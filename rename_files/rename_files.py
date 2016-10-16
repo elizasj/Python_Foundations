5c443f0ae358b67391bfefe43c6a68dcbbcdf333
Eliza Struthers-Jobin <eliza.struthersjobin@gmail.com>
2016-10-16 22:19:32 +0200

    mod path to files

--- rename_files/rename_files.py
+++ rename_files/rename_files.py
@@ -2,15 +2,15 @@ import os
 
 def rename_files():
     # get file names from a folder
-    file_list = os.listdir('files')
+    file_list = os.listdir('YOUR DRIVE HERE')
 
     path = os.getcwd()
 
-    os.chdir('/Users/elizasj/Documents/Python/rename_files/files')
+    os.chdir('/Users/USERNAME/PATH/TO/YOUR/FILES')
 
     # for each file, rename filename
     file_num = 0
-    
+
     for file_name in file_list:
        os.rename(file_name, file_name.translate(None, " "))
        new_filename = "bckground_img_{}.png".format(file_num)
